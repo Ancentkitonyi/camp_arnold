8200b5693f28f9d231c84a341f0ea869062473d4
live-beta <wanjala.sam.s@gmail.com>
2016-12-05 14:35:38 +0300

    Update File With File Read and Write

--- user_interface.py
+++ user_interface.py
@@ -20,3 +20,56 @@ while True:
         break
     else:
         print("You have to enter the numbers 1-6only")
+def reader(self,name):
+    numbers = list()
+    # Open the input text file for reading
+    name= raw_input('Enter Your Name')
+    dataFile = open(name + ".txt",'r')
+
+    for eachLine in dataFile:
+    # setup a temporay variable
+        tmpStr = ''
+
+        for char in eachLine:
+
+            if char.isdigit():
+                # if it is a number add it to the tmpStr
+                tmpStr += char
+
+            elif char == ',' and tmpStr != '':
+                numbers.append(int(tmpStr))
+                tmpStr = ''
+        # if the tmpStr contains a number add it to the
+        # numbers list.
+        if tmpStr.isdigit():
+            numbers.append(int(tmpStr))
+    # Print the number list
+    print numbers
+    # Close the input data file.
+    dataFile.close()
+
+    # 2) Uses the string function split to line from the file
+    # into a list of substrings
+    numbers = list()
+    dataFile = open(name + ".txt", 'r')
+
+    for eachLine in dataFile:
+
+        substrs = eachLine.split(',',eachLine.count(','))
+
+        for strVar in substrs:
+            if strVar.isdigit():
+                numbers.append(int(strVar))
+
+    print numbers
+
+    dataFile.close()
+def writer(self,*args):
+    aList = [1, 1, 1, 4,6, 5, 6, 7, 8, 9, 10,
+    'one', 'two', 'three', 'four', 'five',
+    'six', 'seven', 'eight', 'nine', 'ten']
+    dataFile = open(name + ".txt", 'w')
+    for eachitem in aList:
+        dataFile.write(str(eachitem)+'\n')
+
+    dataFile.close()
